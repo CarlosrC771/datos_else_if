63fbe57cca6e9b605e2aa0fe97bd0267a6dc840e
RcCaRlOs <77160047@certus.edu.pe>
2025-04-30 20:59:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var dato = 20;
+
+if(dato<=0)
+{
+    document.write("El número es menor o igual a 0");
+}
+else if(dato<=5)
+{
+    document.write("El número es mayor que 0 y menor o igual a 5");
+}
+else if(dato<=10)
+{
+    document.write("El número es mayor que 5 y menor o igual a 10");
+}
+else if(dato<=15)
+{
+    document.write("El número es mayor que 10 y menor o igual a 15");
+}
+else if(dato<=20)
+{
+    document.write("El número es mayor que 15 y menor o igual a 20");
+}
+else
+{
+    document.write("El número es mayor que 20");
+}
